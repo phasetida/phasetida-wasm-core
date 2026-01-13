f8fce898853e29a5020f2dd4cb8f408bb403aa9c
nofyso <nofyso@users.noreply.github.com>
2026-01-13 14:01:26 +0000

    deploy: 3f5b7096f5caf4205ebd5276d2011ac6711ea200

---
+++ phasetida_wasm_core.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function load_level(js_value: string): any;
+
+export function pre_draw(time_in_second: number, delta_time_in_second: number, auto: boolean): void;
+
+export function reset_note_state(before_time_in_second: number): void;
---
+++ phasetida_wasm_core.js
@@ -0,0 +1,5 @@
+import * as wasm from "./phasetida_wasm_core_bg.wasm";
+export * from "./phasetida_wasm_core_bg.js";
+import { __wbg_set_wasm } from "./phasetida_wasm_core_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
