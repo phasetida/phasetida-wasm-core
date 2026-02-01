0da378124c10a181584ac1d91c42db01a5c0ced0
nofyso <nofyso@users.noreply.github.com>
2026-02-01 06:19:42 +0000

    deploy: 2d0bd17ddddf0996680840e120188ff4a8a95e58

---
+++ phasetida_wasm_core_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const load_image_offset: (a: number, b: number, c: number, d: number) => void;
+export const load_level: (a: number, b: number) => [number, number, number];
+export const pre_draw: (a: number, b: number, c: number) => void;
+export const reset_note_state: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
